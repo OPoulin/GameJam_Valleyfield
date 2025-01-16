023a93b4031b6e5e1f7743a2ee577fd22ccdd388
massimo <masst123543@gmail.com>
2025-01-16 16:02:43 -0500

    ui alls good

---
+++ gameJam_valleyfield/Assets/scripts/ui/otherUiIbuttons.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class otherUiIbuttons : MonoBehaviour
+{
+    public void boutonOeuil()
+    {
+        print("eye");
+    }
+
+    public void boutonTerminer()
+    {
+        print("end");
+    }
+}
