527ecc1e2f8332fec1eb073d2de8e32a7f0ae9d4
Olivier Poulin <456crdv@gmail.com>
2025-01-16 16:37:45 -0500

    Ajout de la musique du menu

---
+++ gameJam_valleyfield/Assets/sound/Scripts/AllSFX.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllSFX : MonoBehaviour
+{
+    public static string bouton = "event:/SFX/Button";
+    public static string huement = "event:/SFX/Boooo";
+    public static string ding = "event:/SFX/Ding";
+    public static string kaChing = "event:/SFX/Ka-Ching";
+    public static string selection = "event:/SFX/Object Select";
+    public static string paintingSlide = "event:/SFX/Painting Slide";
+
+    /*Les outils*/
+    public static string dessin = "event:/SFX/Use Pencil";
+    public static string gomette = "event:/SFX/Use Gomette";
+    public static string pateAModeler = "event:/SFX/Use Pate A Modeler";
+    public static string marbre = "event:/SFX/Use Marbre";
+    public static string postIt = "event:/SFX/Use Post-it";
+    public static string essuieTout = "event:/SFX/Use Essuie-Tout";
+    public static string scotch = "event:/SFX/Use Scotch";
+
+    /*La cinematique*/
+    public static string cinematique = "event:/Cinematique/cinematique";
+}
