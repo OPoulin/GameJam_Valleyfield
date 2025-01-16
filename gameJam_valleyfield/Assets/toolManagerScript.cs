2d769f70b86da71246731e53a516f897e793f6c4
massimo <masst123543@gmail.com>
2025-01-15 22:01:55 -0500

    tool ui

---
+++ gameJam_valleyfield/Assets/toolManagerScript.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toolManagerScript : MonoBehaviour
+{
+    public static GameObject[] toutesCases = new GameObject[6];
+    public static string selectedToolName;
+    public static int selectedToolIndex;
+}
