6800b2fdff11aac934f691233666d68904954c22
DominicGibeau <119945188+DominicGibeau@users.noreply.github.com>
2025-01-16 16:31:50 -0500

    mob farm

--- gameJam_valleyfield/Assets/scripts/leTimer/timerSkip.cs
+++ gameJam_valleyfield/Assets/scripts/leTimer/timerSkip.cs
@@ -14,6 +14,25 @@ public class timerSkip : MonoBehaviour
     public float seconde;
     public bool aDejaFaitSonTour;
     public bool FINI;
+
+
+    public int laPeinture;
+    public GameObject gestionPeinture;
+
+    public List<int> listeRandom;
+
+    public bool peinture0;
+    public bool peinture1;
+    public bool peinture2;
+    public bool peinture3;
+    public bool peinture4;
+
+
+
+    /// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public bool skip;
+    /// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,12 +41,33 @@ public class timerSkip : MonoBehaviour
         minute = 1;
         // faire rouler le temps
         InvokeRepeating("leTemps", 0, 1f);
+
+
+        prendUnePeinture();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (FINI == true)
+        {
+            FINI = false;
+            aDejaFaitSonTour = false;
+            minute = 1;
+            prendUnePeinture();
+            
+
+        }
+
+        if(skip == true)
+        {
+            minute = 1;
+            seconde = 1;
+            prendUnePeinture();
+            skip = false;
+        }
+
     }
 
     //calcul du temps
@@ -46,20 +86,9 @@ public class timerSkip : MonoBehaviour
                 seconde = seconde - 1;
             }
 
-            if(seconde == 0)
-            {
-                if (aDejaFaitSonTour = true)
-                {
+            if(seconde == 0 && minute == 0)
+            {             
                     FINI = true;
-
-                }
-                if (aDejaFaitSonTour = false)
-                {
-                    seconde = 59;
-                    aDejaFaitSonTour = true;
-                }
-               
-
             }
            
            
@@ -79,6 +108,86 @@ public class timerSkip : MonoBehaviour
         temps.text = $"{minute.ToString().PadLeft(2, '0')}:{seconde.ToString().PadLeft(2, '0')}";
     }
 
+
+
+
+
+    void prendUnePeinture()
+    {
+        laPeinture = Random.Range(0, 5);
+
+        // ///////////// la première peinture ////////////////////////////////////////////////
+        if (laPeinture == 0 && peinture0 == true)
+        {
+            prendUnePeinture();
+            return;
+        }
+        if (laPeinture == 0 && peinture0 == false)
+        {
+            peinture0 = true;
+            gestionPeinture.GetComponent<selectionnerPeinture>().ChangerPeinture(laPeinture);
+            //  Invoke("prendUnePeinture", 5f);
+        }
+
+        // ///////////// la deuxième peinture ////////////////////////////////////////////////
+        if (laPeinture == 1 && peinture1 == true)
+        {
+            //laPeinture = Random.Range(0, 5);
+            prendUnePeinture();
+            return;
+        }
+        if (laPeinture == 1 && peinture1 == false)
+        {
+            peinture1 = true;
+            gestionPeinture.GetComponent<selectionnerPeinture>().ChangerPeinture(laPeinture);
+            //  Invoke("prendUnePeinture", 5f);
+        }
+
+
+        // ///////////// la troisième peinture ////////////////////////////////////////////////
+        if (laPeinture == 2 && peinture2 == true)
+        {
+            //laPeinture = Random.Range(0, 5);
+            prendUnePeinture();
+            return;
+        }
+        if (laPeinture == 2 && peinture2 == false)
+        {
+            peinture2 = true;
+            gestionPeinture.GetComponent<selectionnerPeinture>().ChangerPeinture(laPeinture);
+            //   Invoke("prendUnePeinture", 5f);
+        }
+
+        // ///////////// la quatrième peinture ////////////////////////////////////////////////
+        if (laPeinture == 3 && peinture3 == true)
+        {
+            //laPeinture = Random.Range(0, 5);
+            prendUnePeinture();
+            return;
+        }
+        if (laPeinture == 3 && peinture3 == false)
+        {
+            peinture3 = true;
+            gestionPeinture.GetComponent<selectionnerPeinture>().ChangerPeinture(laPeinture);
+            //  Invoke("prendUnePeinture", 5f);
+        }
+
+        // ///////////// la cinquième peinture ////////////////////////////////////////////////
+        if (laPeinture == 4 && peinture4 == true)
+        {
+            //laPeinture = Random.Range(0, 5);
+            prendUnePeinture();
+            return;
+        }
+        if (laPeinture == 4 && peinture4 == false)
+        {
+            peinture4 = true;
+            gestionPeinture.GetComponent<selectionnerPeinture>().ChangerPeinture(laPeinture);
+            //Invoke("prendUnePeinture", 5f);
+        }
+
+
+    }
 
 
 }
--- gameJam_valleyfield/Assets/scripts/randomiser.cs
+++ gameJam_valleyfield/Assets/scripts/randomiser.cs
@@ -8,15 +8,21 @@ public class randomiser : MonoBehaviour
     public GameObject gestionPeinture;
 
     public List<int> listeRandom;
- 
+
+    public bool peinture0;
+    public bool peinture1;
+    public bool peinture2;
+    public bool peinture3;
+    public bool peinture4;
+
 
     // Start is called before the first frame update
     void Start()
     {
-       laPeinture = Random.Range(0, 5);
-        print(laPeinture);
-        gestionPeinture.GetComponent<selectionnerPeinture>().ChangerPeinture(laPeinture);
        
+        print(laPeinture);
+       
+        prendUnePeinture();
     }
 
     // Update is called once per frame
@@ -25,4 +31,82 @@ public class randomiser : MonoBehaviour
         
 
     }
+
+   void prendUnePeinture()
+    {
+        laPeinture = Random.Range(0, 5);
+
+        // ///////////// la première peinture ////////////////////////////////////////////////
+        if (laPeinture == 0 && peinture0 == true)
+        {      
+            prendUnePeinture();
+            return;
+        }
+        if (laPeinture == 0 && peinture0 == false)
+        {
+            peinture0 = true;
+            gestionPeinture.GetComponent<selectionnerPeinture>().ChangerPeinture(laPeinture);
+            Invoke("prendUnePeinture", 5f);
+        }
+
+        // ///////////// la deuxième peinture ////////////////////////////////////////////////
+        if (laPeinture == 1 && peinture1 == true)
+        {
+            //laPeinture = Random.Range(0, 5);
+            prendUnePeinture();
+            return;
+        }
+        if (laPeinture == 1 && peinture1 == false)
+        {
+            peinture1 = true;
+            gestionPeinture.GetComponent<selectionnerPeinture>().ChangerPeinture(laPeinture);
+            Invoke("prendUnePeinture", 5f);
+        }
+
+
+        // ///////////// la troisième peinture ////////////////////////////////////////////////
+        if (laPeinture == 2 && peinture2 == true)
+        {
+            //laPeinture = Random.Range(0, 5);
+            prendUnePeinture();
+            return;
+        }
+        if (laPeinture == 2 && peinture2 == false)
+        {
+            peinture2 = true;
+            gestionPeinture.GetComponent<selectionnerPeinture>().ChangerPeinture(laPeinture);
+            Invoke("prendUnePeinture", 5f);
+        }
+
+        // ///////////// la quatrième peinture ////////////////////////////////////////////////
+        if (laPeinture == 3 && peinture3 == true)
+        {
+            //laPeinture = Random.Range(0, 5);
+            prendUnePeinture();
+            return;
+        }
+        if (laPeinture == 3 && peinture3 == false)
+        {
+            peinture3 = true;
+            gestionPeinture.GetComponent<selectionnerPeinture>().ChangerPeinture(laPeinture);
+            Invoke("prendUnePeinture", 5f);
+        }
+
+        // ///////////// la cinquième peinture ////////////////////////////////////////////////
+        if (laPeinture == 4 && peinture4 == true)
+        {
+            //laPeinture = Random.Range(0, 5);
+            prendUnePeinture();
+            return;
+        }
+        if (laPeinture == 4 && peinture4 == false)
+        {
+            peinture4 = true;
+            gestionPeinture.GetComponent<selectionnerPeinture>().ChangerPeinture(laPeinture);
+            Invoke("prendUnePeinture", 5f);
+        }
+
+       
+    }
+
 }
