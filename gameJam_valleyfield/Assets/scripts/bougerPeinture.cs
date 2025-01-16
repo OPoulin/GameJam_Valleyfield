7c1f4e3f68972a6ab7c77ec22052e0501d9d18a5
LohanMout <159461731+LohanMout@users.noreply.github.com>
2025-01-16 14:31:38 -0800

    fonction comparaison a l'aire fonctionnel (?)

--- gameJam_valleyfield/Assets/scripts/bougerPeinture.cs
+++ gameJam_valleyfield/Assets/scripts/bougerPeinture.cs
@@ -1,10 +1,18 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class bougerPeinture : MonoBehaviour
 {
     public float vitesse = 1f;
     public float vitesseDeplace = 1f;
     public bool caPart;
+    public bool cestParti;
+
+    public GameObject dessin;
+    public GameObject check;
+    public GameObject OG;
+    public GameObject placeholder;
+
     void Update()
     {
         float l = -vitesse * Input.GetAxis("Mouse Y");
@@ -24,6 +32,27 @@ public class bougerPeinture : MonoBehaviour
         if(caPart && transform.position.x < 30)
         {
             transform.Translate(new Vector3(vitesseDeplace * 1.2f, 0f, 0f));
+            if (!cestParti)
+            {
+                ResizeDessin();
+            }
+            cestParti = true;
         }
+    }
+
+    public void ResizeDessin()
+    {
+        Texture2D texture = dessin.GetComponent<MeshRenderer>().material.mainTexture as Texture2D;
+        Texture2D nouveauAlbedo = ResizeTexture(texture, 1024, 1024);
+        placeholder.GetComponent<MeshRenderer>().material.mainTexture = nouveauAlbedo;
+        gameObject.GetComponent<CompareTextures>().PerformComparison(check, OG, placeholder);
+    }
+
+    Texture2D ResizeTexture(Texture2D original, int width, int height)
+    {
+        Texture2D nouvelleTexture = new Texture2D(width, height);
+        Graphics.ConvertTexture(original, nouvelleTexture);
+        nouvelleTexture.Apply();
+        return nouvelleTexture;
     }
 }
